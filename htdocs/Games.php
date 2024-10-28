ce52d4200ba91b6fd5ce9cb1da0bd45a838ee2d6
Clifford Kent S. Aganap <aganap.cliffordkent@ici.edu.ph>
2024-10-28 03:06:00 +0000

    Tiwasananinyo

--- htdocs/Games.php
+++ htdocs/Games.php
@@ -7,17 +7,17 @@ include("header.php");
  <link rel="stylesheet" href="home.css">
 <body>  
         <h1>International Youth Soccer League</h1>
-        <h2 align="center">Recent Games </h2>
+        <h2 align="center">Latest Games </h2>
         <table border="1" align="center" cellspacing="0" cellpadding="10">
         <tr>
             <th>Game id</th>
             <th>Date</th>
             <th>Time</th>
             <th>Location</th>
-            <th>Home team id</th>
-            <th>Away team id</th>
+            <th>Home team </th>
+            <th>Away team </th>
             <th>Home score</th>
-            <th>Away team id</th>
+            <th>Away score</th>
         </tr>
 
         <?php
@@ -33,10 +33,10 @@ include("header.php");
                 echo "<td>" . $result["Date"] . "</td>";
                 echo "<td>" . $result["Time"] . "</td>";
                 echo "<td>" . $result["Location"] . "</td>";
-                echo "<td>" . $result["home_score"] . "</td>";
-                echo "<td>" . $result["away_score"] . "</td>";
                 echo "<td>" . $result["Home_team_id"] . "</td>";
                 echo "<td>" . $result["Away_team_id"] . "</td>";
+                echo "<td>" . $result["home_score"] . "</td>";
+                echo "<td>" . $result["away_score"] . "</td>";
                 echo "</tr>";
             }
         }
--- htdocs/Player_list.php
+++ htdocs/Player_list.php
@@ -14,10 +14,39 @@
 </head>
 <body>
     <h1 class="title">PLAYERS</h1>
+    
+    <table border="1" align="center" cellspacing="0" cellpadding="5">
+        <form method="post">
+            <tr>
+                <th>
+                    <select name="student_lastname">
+                        <option value=""> -- SELECT A PLAYER --</option>
+                        <?php
+                            $sql = "SELECT * FROM Players ORDER BY Last_name ASC";
+                            $query = mysqli_query($conn, $sql);
+                            if (!$query) {
+                                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                            } else {
+                                while ($result = mysqli_fetch_assoc($query)) {
+                                    echo "<option value='{$result['Player_id']}'>{$result['Last_name']}, {$result['First_name']}</option>";
+                                }
+                            }
+                        ?>
+                    </select>
+                </th>
+            </tr>
+            <tr>
+                <th>
+                    <button type="submit" class="button green" name='process_edit'>Edit</button>
+                    <button type="submit" class="button red" name='process_delete'>Delete</button>
+                </th>
+            </tr>
+        </form>
+    </table>
 
     <div class="button-container">
         <button><a href="Addplayer.php">Add player</a></button>
-        <button class="redd"><a href="Delete.php">Delete player</a></button>
+       
     </div>
 
     
@@ -28,13 +57,13 @@
         
         <thead>
             <tr>
-                <th>Player Id</th>
+            
                 <th>Full Name</th>
                 <th>Date of Birth</th>
                 <th>Email</th>
                 <th>Contact Number</th>
-                <th>Team Id</th>
-                <th>Action</th>
+                
+               
             </tr>
         </thead>
         <tbody>
@@ -46,14 +75,14 @@
                 } else {
                     while ($result = mysqli_fetch_assoc($query)) {
                         echo "<tr>";
-                        echo "<td>" . ($result["Player_id"]) . "</td>";
+                       
                         echo "<td>" .($result["Last_name"]) . "," . ($result["First_name"]) . "," . ($result["Middle_name"]) ."</td>";
                         
                         echo "<td>" . date("F d, Y", strtotime($result["Date_of_birth"])) . "</td>";
                         echo "<td>" . ($result["Email"]) . "</td>";
                         echo "<td>" . ($result["Contact_number"]) . "</td>";
-                        echo "<td>" . ($result["Team_id"]) . "</td>";
-                        echo"<td> Kupal kaba boss? </td> ";
+                       
+                       
                         echo "</tr>";
                     }
                 }
